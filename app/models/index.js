d7c32812068d5e7118631a6d7fabb6faf230b9d8
rphlmnbt <rphlmnbt@gmail.com>
2021-08-07 17:56:06 +0800

    Fixed Timezone

--- app/models/index.js
+++ app/models/index.js
@@ -5,7 +5,10 @@ const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
   host: dbConfig.HOST,
   dialect: dbConfig.dialect,
   operatorsAliases: false,
-
+  dialectOptions: {
+    useUTC: false, // for reading from database
+  },
+  timezone: '+08:00', // for writing to database
   pool: {
     max: dbConfig.pool.max,
     min: dbConfig.pool.min,
