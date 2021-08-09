1d37e0858960a6ca68f4aa95a56f70f3adc2b206
rphlmnbt <rphlmnbt@gmail.com>
2021-08-09 10:09:38 +0800

    Allow others to connect aside from localhost. Changed port and added it to dotenv.

--- server.js
+++ server.js
@@ -45,7 +45,7 @@ require('./app/routes/id.routes')(app);
 require('./app/routes/vaccinecard.routes')(app);
 
 // set port, listen for requests
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
+const PORT = process.env.BACKEND_PORT || 8080;
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server is running on port ${PORT}.`);
 });
